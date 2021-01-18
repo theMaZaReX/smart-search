3155669757ad91c2dc17b4ad55fece4c4dba725a
Dmitry <dmitri446@gmail.com>
2021-01-18 12:09:49 +0300

    add text highlighting for search results

--- scripts.js
+++ scripts.js
@@ -1,25 +1,34 @@
-$('#input-search').keyup(function(){
-    let searchStr =  $('#input-search').val();
+$('#input-search').keyup(function () {
+    let $searchStr = $('#input-search').val();
     const search = $('#search');
 
-    if (searchStr.length>=2){
+    if ($searchStr.length >= 2) {
         $.ajax({
             url: '/search.php',
             type: 'POST',
-            data: {'searchRequest': searchStr},
+            data: {'searchRequest': $searchStr},
             dataType: 'html',
-            success: function(data){
+            success: function (data) {
                 const resultJson = JSON.parse(data);
 
-                for (key in search){
-                    while (search[key].firstChild) {
-                        search[key].removeChild(search[key].firstChild);
+                search.each(function (i, item) {
+                    while (item.firstChild) {
+                        item.removeChild(item.firstChild);
                     }
-                }
+                })
 
                 resultJson.forEach(function (element) {
                     search.append(`<div class="search-results__item">${element.name}</div>`)
                 })
+
+                if (search.length != 0) {
+                    $('.search-results__item').each(function (i, resultItem) {
+                        let index = resultItem.textContent.toLowerCase().indexOf($searchStr);
+                        let lastIndex = index + $searchStr.length;
+                        let subStr = resultItem.textContent.substring(index, lastIndex);
+                        resultItem.innerHTML = resultItem.innerHTML.replace(subStr, `<strong>${subStr}</strong>`)
+                    });
+                }
 
             }
         })
