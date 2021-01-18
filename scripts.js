1f4dc011f3fe13ed4ae03748f6b1972074b49a42
Dmitry <dmitri446@gmail.com>
2021-01-18 12:41:49 +0300

    price and quantity added to search results

--- scripts.js
+++ scripts.js
@@ -1,36 +1,56 @@
-$('#input-search').keyup(function () {
-    let $searchStr = $('#input-search').val();
-    const search = $('#search');
+const $search = $('#search');
 
-    if ($searchStr.length >= 2) {
+
+const outputSearchResults = function(data){
+    const resultJson = JSON.parse(data);
+
+    clearSearchResults();
+
+    resultJson.forEach(function (element) {
+        $search.append(`<div class="search-results__item">${element.name}<span class="search-results__item-price">Цена: ${element.price} руб.</span><span class="search-results__item-count">Кол-во: ${element.number} шт.</span></div>`)
+ })
+
+    setHighlightText();
+
+
+}
+
+const setHighlightText = function(){
+    const $searchStr = $('#input-search').val();
+    if ($search.length != 0) {
+        $('.search-results__item').each(function (i, resultItem) {
+            let index = resultItem.textContent.toLowerCase().indexOf($searchStr);
+            let lastIndex = index + $searchStr.length;
+            let subStr = resultItem.textContent.substring(index, lastIndex);
+            resultItem.innerHTML = resultItem.innerHTML.replace(subStr, `<strong>${subStr}</strong>`)
+        });
+    }
+}
+
+const clearSearchResults = function (){
+    $search.each(function (i, item) {
+        while (item.firstChild) {
+            item.removeChild(item.firstChild);
+        }
+    })
+}
+
+$('#input-search').keyup(function () {
+
+    const $searchStr = $(this).val();
+    if ($searchStr.length != 0) {
         $.ajax({
             url: '/search.php',
             type: 'POST',
             data: {'searchRequest': $searchStr},
             dataType: 'html',
             success: function (data) {
-                const resultJson = JSON.parse(data);
-
-                search.each(function (i, item) {
-                    while (item.firstChild) {
-                        item.removeChild(item.firstChild);
-                    }
-                })
-
-                resultJson.forEach(function (element) {
-                    search.append(`<div class="search-results__item">${element.name}</div>`)
-                })
-
-                if (search.length != 0) {
-                    $('.search-results__item').each(function (i, resultItem) {
-                        let index = resultItem.textContent.toLowerCase().indexOf($searchStr);
-                        let lastIndex = index + $searchStr.length;
-                        let subStr = resultItem.textContent.substring(index, lastIndex);
-                        resultItem.innerHTML = resultItem.innerHTML.replace(subStr, `<strong>${subStr}</strong>`)
-                    });
-                }
-
+                outputSearchResults(data, $searchStr);
             }
         })
     }
+    else{
+        clearSearchResults();
+    }
+
 })
